40c177f8444cf9405828d58266cb440f85177a4c
malinamolnar <66071014+malinamolnar@users.noreply.github.com>
2020-05-28 18:38:20 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -21,7 +21,7 @@ mail_settings = {
     "MAIL_USE_TLS": False,
     "MAIL_USE_SSL": True,
     "MAIL_USERNAME": "bookchain.message@gmail.com",
-    "MAIL_PASSWORD": "nijgzfbukjlydtns"	
+    "MAIL_PASSWORD": "**********"	
 	}
 app.config.update(mail_settings)
 mail = Mail(app)
